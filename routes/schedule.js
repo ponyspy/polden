8d2ae9bade64dc20841b890ea88f85e21d4828bf
ponyspy <desade4me@gmail.com>
2014-10-05 17:17:45 +0400

    schedule

--- routes/schedule.js
+++ routes/schedule.js
@@ -1,5 +1,34 @@
+var Event = require('../models/main.js').Event;
+
 exports.main = function(req, res) {
-	res.render('schedule');
+	Event.aggregate()
+	.unwind('schedule')
+  .group({
+    '_id': {
+      year: { $year: '$schedule' },
+      month: { $month: '$schedule' },
+      day: { $dayOfMonth: '$schedule' }
+    },
+    'events': {
+      $push: {
+        title: '$title',
+        hall: '$hall',
+        age: '$age',
+        description: '$description',
+        _id: '$_id',
+        time: {
+          hours: { $hour: '$schedule' },
+          minutes: { $minute: '$schedule' }
+        }
+      }
+    },
+    'count': { $sum: 1 }
+  })
+  .sort({'_id.year': 1, '_id.month': 1, '_id.day': 1})
+  .exec(function(err, dates) {
+  	console.log(dates)
+    res.render('schedule', {dates: dates});
+  });
 }
 
 exports.events = function(req, res) {
