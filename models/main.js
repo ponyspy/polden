c0def6f2772be1ba5d8a41884e62b21e3fbaf37a
ponyspy <desade4me@gmail.com>
2015-04-17 13:42:19 +0300

    schema

--- models/main.js
+++ models/main.js
@@ -1,53 +1,58 @@
 var mongoose = require('mongoose'),
-			Schema = mongoose.Schema;
+		mongooseLocale = require('mongoose-locale'),
+		Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-		login: String,
-		password: String,
-		email: String,
-		status: {type: String, default: 'User'},
-		date: {type: Date, default: Date.now},
+	login: String,
+	password: String,
+	email: String,
+	status: {type: String, default: 'User'},
+	date: {type: Date, default: Date.now},
 });
 
 var newsSchema = new Schema({
-	title: {
-		ru: String,
-		en: String
-	},
-	s_title: {
-		ru: String,
-		en: String
-	},
-	description: {
-		ru: String,
-		en: String
-	},
-	category: String,
-	photo: String,
+	title: { type: String, trim: true, locale: true },
+	s_title: { type: String, trim: true, locale: true },
+	description: { type: String, trim: true, locale: true },
+	photos: [String],
+	date: {type: Date, default: Date.now}
+});
+
+var exhibitionSchema = new Schema({
+	title: { type: String, trim: true, locale: true },
+	description: { type: String, trim: true, locale: true },
+	events: [{ type: Schema.Types.ObjectId, ref: 'Event' }],
 	date: {type: Date, default: Date.now}
 });
 
 var eventSchema = new Schema({
-	title: {
-		ru: String,
-		en: String
+	title: { type: String, trim: true, locale: true },
+	description: { type: String, trim: true, locale: true },
+	meta: {
+		hall: { type: String, trim: true, locale: true },
+		age: { type: String, trim: true, locale: true },
 	},
-	hall: {
-		ru: String,
-		en: String
-	},
-	description: {
-		ru: String,
-		en: String
-	},
-	age: {
-		ru: String,
-		en: String
-	},
-	category: String,
+	category: [{ type: Schema.Types.ObjectId, ref: 'Category' }],
 	schedule: [Date],
 	date: {type: Date, default: Date.now}
 });
+
+var categorySchema = new Schema({
+	title: { type: String, trim: true, locale: true },
+	description: { type: String, trim: true, locale: true },
+	date: {type: Date, default: Date.now}
+});
+
+
+// ------------------------
+// *** Plugins Block ***
+// ------------------------
+
+
+newsSchema.plugin(mongooseLocale);
+exhibitionSchema.plugin(mongooseLocale);
+eventSchema.plugin(mongooseLocale);
+categorySchema.plugin(mongooseLocale);
 
 
 // ------------------------
@@ -57,4 +62,6 @@ var eventSchema = new Schema({
 
 module.exports.User = mongoose.model('User', userSchema);
 module.exports.News = mongoose.model('News', newsSchema);
+module.exports.Event = mongoose.model('Exhibition', exhibitionSchema);
 module.exports.Event = mongoose.model('Event', eventSchema);
+module.exports.Event = mongoose.model('Category', categorySchema);
