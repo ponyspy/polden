a161b94a6434270ff6eff50ea39a78d846fb317b
ponyspy <desade4me@gmail.com>
2015-04-18 15:50:39 +0300

    forms

--- routes/admin/exhibitions.js
+++ routes/admin/exhibitions.js
@@ -1,4 +1,5 @@
 var Exhibition = require('../../models/main.js').Exhibition;
+var Category = require('../../models/main.js').Category;
 
 
 // ------------------------
@@ -8,17 +9,17 @@ var Exhibition = require('../../models/main.js').Exhibition;
 
 var checkNested = function (obj, layers) {
 
-  if (typeof layers == 'string') {
-    layers = layers.split('.');
-  }
+	if (typeof layers == 'string') {
+		layers = layers.split('.');
+	}
 
-  for (var i = 0; i < layers.length; i++) {
-    if (!obj || !obj.hasOwnProperty(layers[i])) {
-      return false;
-    }
-    obj = obj[layers[i]];
-  }
-  return true;
+	for (var i = 0; i < layers.length; i++) {
+		if (!obj || !obj.hasOwnProperty(layers[i])) {
+			return false;
+		}
+		obj = obj[layers[i]];
+	}
+	return true;
 }
 
 
@@ -28,9 +29,9 @@ var checkNested = function (obj, layers) {
 
 
 exports.list = function(req, res) {
-  Exhibition.find().exec(function(err, exhibitions) {
-    res.render('auth/exhibitions/', {exhibitions: exhibitions});
-  });
+	Exhibition.find().exec(function(err, exhibitions) {
+		res.render('auth/exhibitions/', {exhibitions: exhibitions});
+	});
 }
 
 
@@ -40,27 +41,33 @@ exports.list = function(req, res) {
 
 
 exports.add = function(req, res) {
-  res.render('auth/exhibitions/add.jade');
+	Category.find().exec(function(err, categorys) {
+		res.render('auth/exhibitions/add.jade', {categorys: categorys});
+	});
 }
 
 exports.add_form = function(req, res) {
-  var post = req.body;
+	var post = req.body;
 
-  var exhibition = new Exhibition();
+	var exhibition = new Exhibition();
 
-  var locales = post.en ? ['ru', 'en'] : ['ru'];
+	var locales = post.en ? ['ru', 'en'] : ['ru'];
 
-  locales.forEach(function(locale) {
-    checkNested(post, [locale, 'title'])
-      && exhibition.setPropertyLocalised('title', post[locale].title, locale);
+	locales.forEach(function(locale) {
+		checkNested(post, [locale, 'title'])
+			&& exhibition.setPropertyLocalised('title', post[locale].title, locale);
 
-    checkNested(post, [locale, 'description'])
-      && exhibition.setPropertyLocalised('description', post[locale].description, locale);
-  });
+		checkNested(post, [locale, 'description'])
+			&& exhibition.setPropertyLocalised('description', post[locale].description, locale);
+	});
 
-  exhibition.save(function(err, exhibition) {
-    res.redirect('/auth/exhibitions');
-  });
+	exhibition.interval.start = new Date(post.date_start.year, post.date_start.month, post.date_start.date);
+	exhibition.interval.end = new Date(post.date_end.year, post.date_end.month, post.date_end.date);
+	exhibition.categorys = post.categorys;
+
+	exhibition.save(function(err, exhibition) {
+		res.redirect('/auth/exhibitions');
+	});
 }
 
 
@@ -70,33 +77,39 @@ exports.add_form = function(req, res) {
 
 
 exports.edit = function(req, res) {
-  var id = req.params.id;
+	var id = req.params.id;
 
-  Exhibition.findById(id).exec(function(err, exhibition) {
-    res.render('auth/exhibitions/edit.jade', {exhibition: exhibition});
-  });
+	Exhibition.findById(id).exec(function(err, exhibition) {
+		Category.find().exec(function(err, categorys) {
+			res.render('auth/exhibitions/edit.jade', {exhibition: exhibition, categorys: categorys});
+		});
+	});
 }
 
 exports.edit_form = function(req, res) {
-  var post = req.body;
-  var id = req.params.id;
+	var post = req.body;
+	var id = req.params.id;
 
-  Exhibition.findById(id).exec(function(err, exhibition) {
+	Exhibition.findById(id).exec(function(err, exhibition) {
 
-    var locales = post.en ? ['ru', 'en'] : ['ru'];
+		var locales = post.en ? ['ru', 'en'] : ['ru'];
 
-    locales.forEach(function(locale) {
-      checkNested(post, [locale, 'title'])
-        && exhibition.setPropertyLocalised('title', post[locale].title, locale);
+		locales.forEach(function(locale) {
+			checkNested(post, [locale, 'title'])
+				&& exhibition.setPropertyLocalised('title', post[locale].title, locale);
 
-      checkNested(post, [locale, 'description'])
-        && exhibition.setPropertyLocalised('description', post[locale].description, locale);
-    });
+			checkNested(post, [locale, 'description'])
+				&& exhibition.setPropertyLocalised('description', post[locale].description, locale);
+		});
 
-    exhibition.save(function(err, exhibition) {
-      res.redirect('/auth/exhibitions');
-    });
-  });
+		exhibition.interval.start = new Date(post.date_start.year, post.date_start.month, post.date_start.date);
+		exhibition.interval.end = new Date(post.date_end.year, post.date_end.month, post.date_end.date);
+		exhibition.categorys = post.categorys;
+
+		exhibition.save(function(err, exhibition) {
+			res.redirect('/auth/exhibitions');
+		});
+	});
 }
 
 
@@ -106,8 +119,8 @@ exports.edit_form = function(req, res) {
 
 
 exports.remove = function(req, res) {
-  var id = req.body.id;
-  Exhibition.findByIdAndRemove(id, function() {
-    res.send('ok');
-  });
+	var id = req.body.id;
+	Exhibition.findByIdAndRemove(id, function() {
+		res.send('ok');
+	});
 }
--- routes/schedule.js
+++ routes/schedule.js
@@ -1,47 +1,73 @@
 var Event = require('../models/main.js').Event;
 
 exports.main_test = function(req, res) {
-  var start = new Date(Date.UTC(2014, 0, 1));
-  var end = new Date(Date.UTC(2014, 11, 30));
+	var start = new Date(Date.UTC(2014, 0, 1));
+	var end = new Date(Date.UTC(2014, 11, 30));
 
 	Event.aggregate()
-	.unwind('schedule')
-  .sort({'schedule': 1})
-  .match({
-    'schedule': {
-      $gte: start,
-      $lte: end
-    }
-  })
-  .group({
-    '_id': {
-      year: { $year: '$schedule' },
-      month: { $month: '$schedule' },
-      date: { $dayOfMonth: '$schedule' },
-      day: { $dayOfWeek: '$schedule' }
-    },
-    'events': {
-      $push: {
-        title: '$title',
-        hall: '$hall',
-        age: '$age',
-        hall: '$hall',
-        time: {
-          hours: { $hour: '$schedule' },
-          minutes: { $minute: '$schedule' }
-        }
-      }
-    },
-    'count': { $sum: 1 }
-  })
-  .sort({'_id.year': 1, '_id.month': 1, '_id.date': 1})
-  .exec(function(err, dates) {
-    res.render('schedule/test_index.jade', {dates: dates});
-  });
+		.match({'exhibition': req.params.id})
+		.unwind('schedule')
+		.sort({'schedule': 1})
+		.match({
+			'schedule': {
+				$gte: start,
+				$lte: end
+			}
+		})
+		.group({
+			'_id': {
+				year: { $year: '$schedule' },
+				month: { $month: '$schedule' },
+				date: { $dayOfMonth: '$schedule' },
+				day: { $dayOfWeek: '$schedule' }
+			},
+			'events': {
+				$push: {
+					title: '$title',
+					hall: '$hall',
+					age: '$age',
+					time: {
+						hours: { $hour: '$schedule' },
+						minutes: { $minute: '$schedule' }
+					}
+				}
+			},
+			'count': { $sum: 1 }
+		})
+		.sort({'_id.year': 1, '_id.month': 1, '_id.date': 1})
+		.exec(function(err, dates) {
+			res.render('schedule/test_index.jade', {dates: dates});
+		});
 }
 
 exports.events_test = function(req, res) {
-	res.render('schedule/test_events.jade');
+	Event.aggregate()
+		.match({'exhibition': req.params.id})
+		.unwind('categorys')
+		.sort({'schedule': 1})
+		.group({
+			'_id': {
+				'category': '$categorys'
+			},
+			'events': {
+				$push: {
+					title: '$title',
+					hall: '$hall',
+					age: '$age',
+					date: {
+						month: { $month: '$schedule' },
+						date: { $dayOfMonth: '$schedule' },
+					},
+					time: {
+						hours: { $hour: '$schedule' },
+						minutes: { $minute: '$schedule' }
+					}
+				}
+			}
+		})
+		.exec(function(err, categorys) {
+			res.render('schedule/test_events.jade', {categorys: categorys});
+		});
 }
 
 exports.main = function(req, res) {
