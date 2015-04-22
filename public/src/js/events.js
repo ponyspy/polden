3ce0b943b5479dcffdd1cf4916f84373187b9538
ponyspy <desade4me@gmail.com>
2015-04-22 17:40:24 +0300

    fade

--- public/src/js/events.js
+++ public/src/js/events.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
 	$('.category_item').click(function(event) {
 		var current = $(this).attr('class').split(' ')[1];
-		current == 'all'
-			? $('.events_group').removeAttr('style')
-			: $('.events_group').css('opacity', 1).not('.' + current).css('opacity', 0.2);
+		if (current == 'all') {
+			$('.events_group').fadeTo(400, 1)
+		}
+		else {
+			$('.' + current).fadeTo(400, 1);
+			$('.events_group').not('.' + current).fadeTo(400, 0.2);
+		}
 	});
 });
