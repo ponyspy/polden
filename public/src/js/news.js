1c0e8041e63188a61403555b1915b9528b8884b3
ponyspy <desade4me@gmail.com>
2015-04-23 18:13:05 +0300

    scroll

--- public/src/js/news.js
+++ public/src/js/news.js
@@ -1,13 +1,24 @@
 $(document).ready(function() {
 	$('.gallery_navigate.back').on('click', function(event) {
-			$(this).closest('.images_gallery_block').children('.images_gallery_outer').animate({
+			$(this).closest('.images_gallery_block').find('.images_gallery_outer').animate({
 					'scrollLeft': '-=' + 300
 			}, 300);
 	});
 
 	$('.gallery_navigate.forward').on('click', function(event) {
-			$(this).closest('.images_gallery_block').children('.images_gallery_outer').animate({
+			$(this).closest('.images_gallery_block').find('.images_gallery_outer').animate({
 					'scrollLeft': '+=' + 300
 			}, 300);
+	});
+
+	$('.column').on('click', function(event) {
+		var img = $(this).attr('src');
+		$('.news_date_block').hide();
+		$('.image_preview').show().attr('src', img);
+	});
+
+	$('.image_preview').on('click', function() {
+		$(this).hide();
+		$('.news_date_block').show();
 	});
 });
