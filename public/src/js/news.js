eaf8d2e53e0b115256d8f01d0e5652bf26877a18
ponyspy <desade4me@gmail.com>
2015-05-09 14:53:43 +0300

    init

--- public/src/js/news.js
+++ public/src/js/news.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
 	var skip = 6;
+	var $images;
 
 	$(document)
+		.on('mouseup', function(event) {
+			var container = $('.image_preview');
+
+			if (!container.is(event.target)
+					&& container.has(event.target).length === 0)
+			{
+					container.fadeOut(300);
+					$('.main_block').removeAttr('style');
+			}
+		})
 		.on('click', '.column', function(event) {
 			var path = $(this).attr('full');
-			var img = $('<img/>', {src: path});
-
-			$(this).closest('.images_gallery_block').find('.image_preview').empty().append(img).fadeIn(400);
+			$('.main_block').css('opacity', 0.1)
+			$('.image_preview').attr('src', path).fadeIn(300);
 		})
 		.on('click', '.gallery_navigate', function(event) {
 			var $this = $(this);
@@ -21,13 +31,8 @@ $(document).ready(function() {
 			}, 300);
 		})
 		.on('click', '.image_preview', function(event) {
-			$(this).hide();
-			$('.news_date_block').show();
-		})
-		.on('mousemove', '.image_preview', function(event) {
-			var $this = $(this);
-
-			$this.scrollTop(event.pageY - $this.offset().top);
+			$(this).fadeOut(300);
+			$('.main_block').removeAttr('style');
 		})
 		.on('scroll', function() {
 			if ($('.content_block').height() - 650 <= $(document).scrollTop()) {
